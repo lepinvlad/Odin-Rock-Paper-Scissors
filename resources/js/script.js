83ed29e5e41294cd1a8f1e446ae1eb40d9730180
lepinvlad <lepinvladislavbb@gmail.com>
2024-10-30 13:43:09 +0300

    fix relative bug

--- resources/js/script.js
+++ resources/js/script.js
@@ -152,12 +152,12 @@ function updateResult() {
   if (userPoints == 5) {
     setTimeout(() => {
       alert("Game Finished!\nYou won!");
-      window.location.href = "https://lepinvlad.github.io/Odin-Rock-Paper-Scissors/index.html";
+      window.location.href = "https://lepinvlad.github.io/Odin-Rock-Paper-Scissors";
     }, 100);
   } else if (computerPoints == 5) {
     setTimeout(() => {
       alert("Game Finished!\nYou lost!");
-      window.location.href = "https://lepinvlad.github.io/Odin-Rock-Paper-Scissors/index.html";
+      window.location.href = "https://lepinvlad.github.io/Odin-Rock-Paper-Scissors";
     }, 100);
   }
 }
